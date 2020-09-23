68e741d185eff085f3d413a6bb9a4c05947f8c2b
Elvis <elvis_tech@hotmail.com>
2020-09-23 15:30:25 -0300

    one

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ambiente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
